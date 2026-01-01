8329b944df7dc5adf19424970f075c96a971de21
Yeah114 <1246517085@qq.com>
2026-01-01 21:51:48 +0800

    ipc

--- protocol/serve.go
+++ protocol/serve.go
@@ -1,18 +1,51 @@
 package protocol
 
 import (
+	"sync"
+
 	"github.com/hashicorp/go-plugin"
 
 	"github.com/Yeah114/tempest-plugin-sdk/api"
 )
 
+var (
+	serveMu          sync.RWMutex
+	serveTestConfig  *plugin.ServeTestConfig
+)
+
+// SetServeTestConfig enables go-plugin "test mode" for subsequent Serve calls.
+// It is used by the host when running plugins in-process (e.g. via yaegi).
+// restore resets the previous config.
+func SetServeTestConfig(cfg *plugin.ServeTestConfig) (restore func()) {
+	serveMu.Lock()
+	prev := serveTestConfig
+	serveTestConfig = cfg
+	serveMu.Unlock()
+	return func() {
+		serveMu.Lock()
+		serveTestConfig = prev
+		serveMu.Unlock()
+	}
+}
+
+func currentServeTestConfig() *plugin.ServeTestConfig {
+	serveMu.RLock()
+	cfg := serveTestConfig
+	serveMu.RUnlock()
+	return cfg
+}
+
 // Serve starts a go-plugin server for the provided plugin implementation.
 // This is intended to be called from a standalone plugin binary.
 func Serve(p api.Plugin) {
-	plugin.Serve(&plugin.ServeConfig{
+	cfg := &plugin.ServeConfig{
 		HandshakeConfig: Handshake,
 		Plugins: map[string]plugin.Plugin{
 			PluginKey: &DynamicRPCPlugin{Impl: p},
 		},
-	})
+	}
+	if tc := currentServeTestConfig(); tc != nil {
+		cfg.Test = tc
+	}
+	plugin.Serve(cfg)
 }
