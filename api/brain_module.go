5ffb679a2f66f0c640ae9a2ddf76eb033a7fabe1
Yeah114 <1246517085@qq.com>
2026-01-10 21:52:20 +0800

    brain

--- api/brain_module.go
+++ api/brain_module.go
@@ -9,6 +9,7 @@ import (
 type BrainModule interface {
 	Name() string
 
+	// 第1次启用的时候会调用 daemon 的 Load，之后重复启用仅会调用 ReConfig
 	EnableDaemon(ctx context.Context, name string, config map[string]interface{}) (actualConfig map[string]interface{}, daemon define.Daemon, err error)
 	DisableDaemon(ctx context.Context, name string) (err error)
 }
